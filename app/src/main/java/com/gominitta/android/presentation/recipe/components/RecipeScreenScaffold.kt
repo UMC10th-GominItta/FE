85b2aed0dbf8605bd7b24e3d3ba708fef73f2dbd
ddam66 <ddamddam66@gmail.com>
2026-07-23 20:09:51 +0900

    fix: RecipeScreenScaffold 인셋 이중 적용 제거

--- app/src/main/java/com/gominitta/android/presentation/recipe/components/RecipeScreenScaffold.kt
+++ app/src/main/java/com/gominitta/android/presentation/recipe/components/RecipeScreenScaffold.kt
@@ -7,6 +7,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.foundation.layout.WindowInsets
 
 @Composable
 fun RecipeScreenScaffold(
@@ -20,6 +21,7 @@ fun RecipeScreenScaffold(
         modifier = modifier.fillMaxSize(),
         containerColor = Color.Transparent,
         contentColor = MaterialTheme.colorScheme.onBackground,
+        contentWindowInsets = WindowInsets(0, 0, 0, 0),
         topBar = {
             RecipeTopBar(
                 title = title,
