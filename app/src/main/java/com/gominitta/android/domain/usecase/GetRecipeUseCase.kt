14ee297b0e376315984ed91b3b794d0562d37818
jyoun0330 <yeyun714930@gmail.com>
2026-08-07 16:40:24 +0900

    chore: GetRecipeUseCase.kt 대소문자 중복 추적 경로 정리

--- app/src/main/java/com/gominitta/android/domain/usecase/GetRecipeUseCase.kt
+++ app/src/main/java/com/gominitta/android/domain/usecase/GetRecipeUseCase.kt
@@ -4,8 +4,8 @@ import com.gominitta.android.domain.model.recipe.Recipe
 import com.gominitta.android.domain.repository.RecipeRepository
 import javax.inject.Inject
 
-class GetRecipesUseCase @Inject constructor(
+class GetRecipeUseCase @Inject constructor(
     private val repository: RecipeRepository,
 ) {
-    suspend operator fun invoke(): List<Recipe> = repository.getRecipes()
+    suspend operator fun invoke(recipeId: Long): Recipe = repository.getRecipe(recipeId)
 }
